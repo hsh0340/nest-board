ac0cb6e1671040a3cd7b33a89281a8c9c8a247f4
hsh0340 <hsh0340@naver.com>
2023-09-14 15:19:15 +0900

    컨트롤러 메서드 작성

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
+  // 모든 게시물 가져오는 API
+  @Get()
+  findAllBoards() {
+    return 'find all boards';
+  }
+
+  // 특정 게시물을 가져오는 API
+  @Get(':id')
+  findOneBoard(@Param('id') id: number) {
+    return `find specific board: ${id}`;
+  }
+
+  // 게시물을 생성하는 API
+  @Post()
+  createBoard(@Body() board) {
+    return `create board: ${board}`;
+  }
+
+  // 특정 게시물을 수정하는 API
+  @Put(':id')
+  updateBoard(@Param('id') id: number, @Body() board) {
+    return `update ${id} board: ${board}`;
+  }
+
+  // 특정 게시물을 삭제하는 API
+  @Delete(':id')
+  deleteBoard(@Param('id') id: number) {
+    return 'delete board';
+  }
+}
