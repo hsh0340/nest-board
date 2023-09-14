3a365a13ddafd4a2f025e2400d11f46aef8a2241
hsh0340 <hsh0340@naver.com>
2023-09-14 15:19:38 +0900

    board 더미데이터 작성

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  private boards = [
+    {
+      id: 1,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+    {
+      id: 2,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+    {
+      id: 3,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+    {
+      id: 4,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+    {
+      id: 5,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+    {
+      id: 6,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+    {
+      id: 7,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+    {
+      id: 8,
+      title: '게시물 제목',
+      content: 'hello world',
+    },
+  ];
+}
